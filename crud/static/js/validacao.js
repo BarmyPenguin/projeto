6b8723c193ea963edc868ef3596329180eb6675c
BarmyPenguin <phenrique.10@hotmail.com>
2016-09-29 22:27:33 -0300

    Atualização
    Fiz uma atualização no codigo

--- crud/static/js/validacao.js
+++ crud/static/js/validacao.js
@@ -3,18 +3,28 @@ var area = document.getElementById("area").value;
 var ideia = document.getElementById("ideia").value;
 var tipo = document.getElementById("tipo").value;
 var erro = false;
-if(area === ""  || ideia === "" || tipo ===""){
+if(area === "" ){
 	document.getElementById("area").style.backgroundColor="#ff4040";
-	document.getElementById("ideia").style.backgroundColor="#ff4040";
+	erro=true;
+}else{
+	if(ideia ===""){
+		document.getElementById("ideia").style.backgroundColor="#ff4040";
+		erro=true;
+	} else {
+		if(tipo === ""){
 	document.getElementById("tipo").style.backgroundColor="#ff4040";
 	erro=true;
+		}
+	}
 }
+
+
+
 if(erro==true){
 	alert("Preencha os campos destacados");
 }
+
 }
-
-
 function mudarcor(campo){
 
 	document.getElementById(campo).style.backgroundColor="white";
